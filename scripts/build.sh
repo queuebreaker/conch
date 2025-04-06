7e0b82d9abbfb0f11427baac6f11ef65de491445
queuebreaker <queuebreaker@proton.me>
2025-04-06 17:30:03 +0200

    clean up

--- scripts/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gcc -o build/conch src/conch.c
--- src/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(int argc, char** argv)
-{
-    char* bin_argv[] = {"/tmp/conchbin", NULL};
-    execv(bin_argv[0], bin_argv);
-}
-
